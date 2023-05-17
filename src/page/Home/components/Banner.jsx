d540027dcd43df200d8e93bcbcfc29a3a07d7f91
developer-nieem <naimhossenwork@gmail.com>
2023-05-18 05:57:25 +0600

    banner section done

---
+++ src/page/Home/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='banner-section d-flex justify-content-center align-items-center'>
+            <div className=' text-white text-center'>
+                        <h2>
+                        Rev Up Your Collection with NM Car Toys Shop!
+                        </h2>
+                        <p>
+                        Discover a world of miniature automotive excellence at NM Car Toys Shop.  Explore our vast selection of high-quality <br /> and realistic toy cars, perfect for collectors, enthusiasts, and kids. 
+                        </p>
+                        <button className='btn btn-primary btn-lg'>Purchase Car Toys</button>
+                    
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
