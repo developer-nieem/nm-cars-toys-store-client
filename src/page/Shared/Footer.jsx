c9374414a270701abf57c7e434eca8dbf8193b82
developer-nieem <naimhossenwork@gmail.com>
2023-05-18 00:07:37 +0600

    add social icon on the footer

--- src/page/Shared/Footer.jsx
+++ src/page/Shared/Footer.jsx
@@ -1,4 +1,5 @@
 import logo from '../../../public/logo.png'
+import { FaEnvelope, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 const Footer = () => {
     return (
        
@@ -6,38 +7,39 @@ const Footer = () => {
         <footer style={{backgroundColor: 'rgb(10 16 32)'}} className=" text-white pt-5">
             <div className="container">
           <div className="row mb-5">
-            <div className="col-md-6">
-              <a href="#" > <img className='w-25 mb-3' src={logo } alt="car"  /> </a>
-              <p className='me-5 pe-5'>Welcome to NM Car Toys Shop, your one-stop destination for all your car toy needs. We offer a wide range of high-quality and realistic toy cars that are perfect for collectors, enthusiasts, and children alike. Whether you're looking for classic models, modern sports cars, or vintage replicas, we have something for everyone. </p>
+            <div className="col-md-5">
+              <a href="#" > <img style={{width:'30%'}} className=' mb-3' src={logo } alt="car"  /> </a>
+              <p className=' pe-5'>Welcome to NM Car Toys Shop, your one-stop destination for all your car toy needs. We offer a wide range of high-quality and realistic toy cars that are perfect for collectors, enthusiasts, and children alike. Whether you're looking for classic models, modern sports cars, or vintage replicas, we have something for everyone. </p>
             </div>
-            <div className="col-md-2 ml-auto">
+            <div className="col-md-2 mt-4 ml-auto">
               <h3>Shop</h3>
-              <ul className="list-unstyled links">
-                <li><a href="#">Sell online</a></li>
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Examples</a></li>
-                <li><a href="#">Website editors</a></li>
-                <li><a href="#">Online retail</a></li>
+              <ul className=" links">
+                <li><a >Sell online</a></li>
+                <li><a >Features</a></li>
+                <li><a >Examples</a></li>
+                <li><a >Website editors</a></li>
+                <li><a >Online retail</a></li>
               </ul>
             </div>
-            <div className="col-md-2 ml-auto">
-              <h3>Press</h3>
-              <ul className="list-unstyled links">
-                <li><a href="#">Events</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Awards</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Online retail</a></li>
-              </ul>
-            </div>
-            <div className="col-md-2 ml-auto">
+           
+            <div className="col-md-2 mt-4 ml-auto">
               <h3>About</h3>
+              <ul className=" links">
+                <li><a >Contact</a></li>
+                <li><a >Services</a></li>
+                <li><a >Team</a></li>
+                <li><a >Career</a></li>
+                <li><a >Contacts</a></li>
+              </ul>
+            </div>
+
+            <div className="col-md-3 mt-4 ml-auto">
+              <h3>Contact Us</h3>
               <ul className="list-unstyled links">
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Career</a></li>
-                <li><a href="#">Contacts</a></li>
+                <li>Email: nieemhossen@gmail.com </li>
+                <li>Phone: 01838046483 </li>
+                <li className='fs-4 text-center '> <a className='px-2' href="#"><FaFacebook></FaFacebook></a> <a className='px-2'  href="#"> <FaTwitter></FaTwitter></a> <a className='px-2' href="#"><FaYoutube></FaYoutube></a>  <a className='px-2' href="#"><FaEnvelope></FaEnvelope></a> </li>
+                
               </ul>
             </div>
           </div>
